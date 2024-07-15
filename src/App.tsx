87badbc3821c805eeed89b0eb3b671ad43d12660
nenahui <kanatraccoon@gmail.com>
2024-07-15 16:15:45 +0600

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { Typography } from 'antd';
+
+export const App = () => {
+  return (
+    <div className={'container'}>
+      <Typography.Text>Init app</Typography.Text>
+    </div>
+  );
+};
